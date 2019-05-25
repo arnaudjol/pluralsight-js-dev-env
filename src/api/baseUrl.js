11db34e9796800d65c6187d2a682adb3b895ac74
arnaudjol <anolago@gmail.com>
2019-05-25 21:09:00 +0200

    Production build module

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,4 +1,14 @@
 export default function getBaseUrl() {
-  const inDev = window.location.hostname === 'localhost';
-  return inDev ? 'http://localhost:3001/' : '/';
+  return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : '/';
+}
+
+function getQueryStringParameterByName(name, url) {
+  if(!url) url = window.location.href;
+  name = name.replace(/[\[\]]/g, "\\$&");
+  const regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)");
+  const results = regex.exec(url);
+  if(!results) return null;
+  if(!results[2]) return '';
+
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
